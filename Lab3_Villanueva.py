0a1198e4ffb0b496c37953b83ee1c8cd97ad5e7b
j2hann <villanueva.johann06@gmail.com>
2024-09-23 15:19:22 +0800

    FINAL UPDATED

--- Lab3_Villanueva.py
+++ Lab3_Villanueva.py
@@ -1,17 +1,22 @@
 salary = int(input("Enter your monthly salary: "))
-loan = int(input("Enter your desired loan: "))
-# HI MISS WAIT LANG PO NAKALIMUTAN KO PO YUNG SALARY DETECTION KAPAG 30000 HUHUHU
-loanLimit = salary * 10
-if loan > loanLimit:
-    print("You are not qualified for the loan. Reason: Your loan is 10 times higher than your monthly salary.")
+
+if salary < 30000:
+    print("You are not eligible for a loan. Reason: Your salary is less than 30000")
 else:
-    print("You are eligible for a loan.")
-    months = int(input("How many months to pay?: "))
+    loan = int(input("Enter your desired loan: "))
 
-    interest = loan * 0.10
-    totalPayback = interest + loan
-    monthlyLoanPayment = totalPayback / months
-    monthlyLoanPayment = round(monthlyLoanPayment, 2)
+    loanLimit = salary * 10
 
-    print("You are going to pay " + str(totalPayback) + " within " + str(months) + " months.")
-    print("Your monthly payment is: " + str(monthlyLoanPayment))
+    if loan > loanLimit:
+        print("You are not eligible for a loan. Reason: Your loan is 10 times higher than your monthly salary.")
+    else:
+        print("You are eligible for a loan.")
+        
+        months = int(input("How many months to pay?: "))
+        interest = loan * 0.10
+        totalPayback = interest + loan
+        monthlyLoanPayment = totalPayback / months
+        monthlyLoanPayment = round(monthlyLoanPayment, 2)
+
+        print("You are going to pay " + str(totalPayback) + " within " + str(months) + " months.")
+        print("Your monthly payment is: " + str(monthlyLoanPayment))
